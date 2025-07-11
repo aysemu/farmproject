f4ef69ea32fd939b5bc6ec93191abe8decca0c83
Ayse Nur ARIKDOGAN <aysenurarikdogan@gmail.com>
2025-07-11 10:23:23 +0300

    init

---
+++ src/app/components/main/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+
+
+  return (
+    <div style={{ display: "flex", gap: "10px" }}>
+      
+    </div>
+  );
+}
